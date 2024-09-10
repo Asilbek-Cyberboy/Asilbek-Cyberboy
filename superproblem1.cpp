ecc43ab28442d06a17090c11b253575cb436558e
a.temirov <a.temirov@newuu.uz>
2024-09-10 10:01:51 +0500

    new solution

---
+++ superproblem1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 189;
+    char b = 'B';
+    float num1 = float(a)/4;
+    int num2 = int(b);
+    float num3 = float(a+b)/5;
+    char num4=a/3 -7+b;
+    cout<<num1<<endl;
+    cout<<num2<<endl;
+    cout<<num3<<endl;
+    cout<<int(num4)<<endl;
+    return 0;
+}
